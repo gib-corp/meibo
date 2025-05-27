e6959cfb85ddc7d6fa5c70e5b6d35463740cb2f5
Ugo <j.erghioui@gmail.com>
2025-05-27 22:16:26 +0200

    Gsap

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import "./App.css";
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 function App() {
   return <h1 className="font-sans text-[45px]">Hello World</h1>;
